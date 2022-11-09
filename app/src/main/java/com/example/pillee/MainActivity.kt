ad61ddd4ab950ea3d0a5e9ea0df305fd718ede15
FranciscoJavier <javidominguez@econext.es>
2022-11-09 19:46:39 +0000

    a

--- app/src/main/java/com/example/pillee/MainActivity.kt
+++ app/src/main/java/com/example/pillee/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.pillee
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonClick = findViewById<Button>(R.id.button_click)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, AuthActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
