918d75207ee367cd925d70c293a105ff591d025a
Willy Escovilla <willy.escovilla101@alu.ulpgc.es>
2022-11-09 19:27:27 +0000

    changed changes

--- app/src/main/java/com/example/pillee/AuthActivity.kt
+++ app/src/main/java/com/example/pillee/AuthActivity.kt
@@ -11,28 +11,7 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
-    }
-
-    private fun setup (){
-        title = "Login"
-        loginbutton.setOnClickListener {
-            if (mailfield.text.isNotEmpty() && passwordfield.text.isNotEmpty()) {
-                FirebaseAuth.getInstance().signInWithEmailAndPassword(mailfield.text.toString(),
-                    passwordfield.text.toString()).addOnCompleteListener{
-                    if (it.isSuccessful){
-
-                    }else{
-                        Toast.makeText(this,"Cound't make login", Toast.LENGTH_SHORT).show()
-                    }
-                }
-
-            }
-        }
-    }
-    private fun showSchedule (email:String){
-        val intent = Intent(this,ScheduleFragment::class.java).apply {
-            putExtra("email", email)
-        }
 
     }
+
 }
