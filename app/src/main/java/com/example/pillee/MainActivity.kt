0410ebe348aa4e510614c3d428ddb8f8ad3b91c1
Willy Escovilla <willy.escovilla101@alu.ulpgc.es>
2022-11-10 09:19:10 +0000

    Added compose dependencies

--- app/src/main/java/com/example/pillee/MainActivity.kt
+++ app/src/main/java/com/example/pillee/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.pillee
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        
         button_click.setOnClickListener {
             val intent = Intent(this@MainActivity, Login::class.java)
             startActivity(intent)
