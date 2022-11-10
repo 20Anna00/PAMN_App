1337c95b24f355fd170882e08ff0cc4f7cb775fe
Willy Escovilla <willy.escovilla101@alu.ulpgc.es>
2022-11-10 18:50:48 +0000

    low changes

--- app/src/main/java/com/example/pillee/MainActivity.kt
+++ app/src/main/java/com/example/pillee/MainActivity.kt
@@ -3,15 +3,36 @@ package com.example.pillee
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Texts()
+        }
 
 
+
+    }
+
+    @Composable
+    fun Texts(){
+        Text(text = "Hola Jetpack Compose!")
+    }
+
+    @Preview
+    @Composable
+    fun PreviewTexts(){
+        Texts()
+    }
+
+    fun buttons() {
         login_button.setOnClickListener {
             val intent = Intent(this@MainActivity, Login::class.java)
             startActivity(intent)
