3a86d68626bbd0b77388357f1f0fdfb9b1a42c5c
Willy Escovilla <willy.escovilla101@alu.ulpgc.es>
2022-11-10 08:42:18 +0000

    Added connection with firebase database

--- app/src/main/java/com/example/pillee/Login.kt
+++ app/src/main/java/com/example/pillee/Login.kt
@@ -1,28 +1,29 @@
 package com.example.pillee
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.login.*
+import kotlinx.android.synthetic.main.activity_login.*
 
-class AuthActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
-
+        setContentView(R.layout.activity_login)
+        setup()
     }
-/*
+
     private fun setup (){
         title = "Login"
         loginbutton.setOnClickListener {
-            Toast.makeText(this,"hola", Toast.LENGTH_SHORT).show()
             if (mailfield.text.isNotEmpty() && passwordfield.text.isNotEmpty()) {
                 FirebaseAuth.getInstance().signInWithEmailAndPassword(mailfield.text.toString(),
                     passwordfield.text.toString()).addOnCompleteListener{
                     if (it.isSuccessful){
-                        Toast.makeText(this,"hola", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "Hola, salio bien", Toast.LENGTH_SHORT).show()
                     }else{
                         Toast.makeText(this,"Cound't make login", Toast.LENGTH_SHORT).show()
                     }
@@ -31,13 +32,4 @@ class AuthActivity : AppCompatActivity() {
             }
         }
     }
-
-    private fun showSchedule (email:String){
-        val intent = Intent(this,ScheduleFragment::class.java).apply {
-            putExtra("email", email)
-        }
-
-    }
-    */
-
 }
--- app/src/main/java/com/example/pillee/MainActivity.kt
+++ app/src/main/java/com/example/pillee/MainActivity.kt
@@ -3,16 +3,16 @@ package com.example.pillee
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonClick = findViewById<Button>(R.id.button_click)
-        buttonClick.setOnClickListener {
-            val intent = Intent(this, AuthActivity::class.java)
+
+        button_click.setOnClickListener {
+            val intent = Intent(this@MainActivity, Login::class.java)
             startActivity(intent)
         }
     }
