2cbd8691e9ff7a712ee8baf5d4aa6612b434c666
FranciscoJavier <javidominguez@econext.es>
2022-11-11 18:41:16 +0000

    a

--- app/src/main/java/com/example/pillee/HomeScreen.kt
+++ app/src/main/java/com/example/pillee/HomeScreen.kt
@@ -1,12 +1,8 @@
 package com.example.pillee
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.TextButton
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -14,12 +10,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.pillee.jetpackcompnavigation.navigation.AppScreens
-import com.example.pillee.themes.AquaBlue
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -44,5 +38,9 @@ fun BodyContent(navController: NavController){
         }
     }
 }
-
+@Preview
+@Composable
+fun PreviewHome(){
+    BodyContent(rememberNavController())
+}
 
