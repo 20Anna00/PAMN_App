6c16967c476ce12e5d75c3c9e5c50fc01d65a28a
Willy Escovilla <willy.escovilla101@alu.ulpgc.es>
2022-11-12 20:42:27 +0000

    a

--- app/src/main/java/com/example/pillee/jetpackcompnavigation/screens/HomeScreen.kt
+++ app/src/main/java/com/example/pillee/jetpackcompnavigation/screens/HomeScreen.kt
@@ -1,17 +1,10 @@
 package com.example.pillee.jetpackcompnavigation.screens
 
-<<<<<<< HEAD:app/src/main/java/com/example/pillee/HomeScreen.kt
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material.MaterialTheme
-=======
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
->>>>>>> 59e429a9c7c0bfb3bf50b9611599716f9f78dadf:app/src/main/java/com/example/pillee/jetpackcompnavigation/screens/HomeScreen.kt
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -19,10 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-<<<<<<< HEAD:app/src/main/java/com/example/pillee/HomeScreen.kt
 import androidx.compose.ui.graphics.Color
-=======
->>>>>>> 59e429a9c7c0bfb3bf50b9611599716f9f78dadf:app/src/main/java/com/example/pillee/jetpackcompnavigation/screens/HomeScreen.kt
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
