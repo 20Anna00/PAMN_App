5360c6c3a864495339f73d67e15a71e12ddf34e9
Willy Escovilla <willy.escovilla101@alu.ulpgc.es>
2022-11-14 15:16:08 +0000

    a

---
+++ app/src/main/java/com/example/pillee/jetpackcompnavigation/screens/AppointmentScreen.kt
@@ -0,0 +1,14 @@
+package com.example.pillee.jetpackcompnavigation.screens
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppointmentScreen(navController: NavController){
+    Scaffold() {
+
+    }
+}
