3b594a364e72acdb018c2a07cfee9ddb76b52bb6
Anna Sack <a.sack@tu-braunschweig.de>
2022-12-17 18:41:48 +0100

    merge conflict problems

--- app/src/main/java/com/example/pillee/jetpackcompnavigation/screens/ScheduleScreen.kt
+++ app/src/main/java/com/example/pillee/jetpackcompnavigation/screens/ScheduleScreen.kt
@@ -35,20 +35,12 @@ import com.example.pillee.jetpackcompnavigation.navigation.AppScreens
 import com.example.pillee.jetpackcompnavigation.screens.appointment.AppointmentViewModel
 import com.example.pillee.jetpackcompnavigation.screens.viewmodels.PillDetailViewModel
 import com.example.pillee.themes.*
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.*
 
-/*@AndroidEntryPoint
-@ExperimentalCoroutinesApi
-class ScheduleScreenActivity: AppCompatActivity(){
-    private val viewModel: PillDetailViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            val dataOrException = viewModel.data.value
-        }
-    }
-}*/
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -72,38 +64,131 @@ fun MyUi(navController: NavController, pillViewModel: PillDetailViewModel, appoi
     val currentDay = mCalendar.get(Calendar.DAY_OF_WEEK)
     var listPills = data.data
     var pills = mutableListOf<Pills>()
-    var list = arrayOf( "nothing", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+    var list = arrayOf( "nothing", "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday")
     var morningPills = mutableListOf<Pills>()
+    var morningString = mutableListOf<String>()
     var afternoonPills = mutableListOf<Pills>()
+    var afternoonString = mutableListOf<String>()
     var eveningPills = mutableListOf<Pills>()
+    var eveningString = mutableListOf<String>()
 
 
-   if (listPills != null) {
-        for (pill in listPills){
-            if(pill.days == list[currentDay]){
-                pills.add(pill)
+
+    var currentsplitHour = currentTime.split(":")
+    var currenttime = currentsplitHour[0] + currentsplitHour[1]
+    var currenttimeReal = currenttime.toInt()
+
+
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    val current = LocalDateTime.now().format(formatter)
+    val calendar = Calendar.getInstance()
+
+    val mYear = calendar.get(Calendar.YEAR)
+    val mMonth = calendar.get(Calendar.MONTH)
+    val mDay = calendar.get(Calendar.DAY_OF_MONTH)
+
+    var todaysDate = LocalDate.now()
+
+    var todaysAppointments = mutableListOf<Appointment>()
+    var todaysMorningAppointments = mutableListOf<Appointment>()
+
+    var todaysAfternoonAppointments = mutableListOf<Appointment>()
+
+    var todaysEveningAppointments = mutableListOf<Appointment>()
+
+
+    if (appointmentList != null){
+        for (app in appointmentList){
+            var timeA = Date(app.dateTime.seconds*1000)
+            var timeApp =  timeA.toString()
+            var splitHour = timeApp.split(" ")
+            var needed = splitHour[0] + " " + splitHour[1] + " " + splitHour[2]
+
+            if (timeA.equals(todaysDate)){
+                todaysAppointments.add(app)
             }
+
         }
     }
+
+
+    for (app in todaysAppointments){
+        val timeA = Date(app.dateTime.seconds*1000)
+        val timeApp =  timeA.toString()
+        val splitHour = timeApp.split(" ")
+        val time = splitHour[3]
+        val timeSplit = time.split(":")
+        val timeSplita = timeSplit[0] + timeSplit[1]
+        var timeReal = timeSplita.toInt()
+        if (timeReal < 1100){
+            todaysMorningAppointments.add(app)
+        }
+         else if (timeReal > 1100 && timeReal < 1600){
+            todaysAfternoonAppointments.add(app)
+        } else {
+            todaysEveningAppointments.add(app)
+        }
+
+    }
+    var counter = -1
+
+
+   /*if (listPills != null) {
+        for (pill in listPills){
+            var splitDays = pill.days.split(", ")
+            for (day in splitDays){
+                if(day == list[currentDay]){
+                    pills.add(pill)
+                    counter++
+                    break
+                }
+            }
+
+        }
+    }*/
     var splitHour = currentTime.split(":")
     var time = splitHour[0] + splitHour[1]
+    var neededHour = ""
+        if (listPills != null) {
+            for (pill in listPills){
+                var splitDays = pill.days.split(", ")
+                for (day in splitDays) {
+                    if (day == list[currentDay]) {
+                        pills.add(pill)
+                        counter++
+                        break
+                    }
+                }
 
 
-    for (pill in pills){
-        var splitHourPill = pill.hour.split(":")
+            var neededHourSplit = pill.hour.split(",")
+
+        if (counter == 0){
+            neededHour = neededHourSplit[0]
+        }
+        if (counter == 1){
+            neededHour = neededHourSplit[1]
+        }
+        if (counter == 2){
+            neededHour = neededHourSplit[2]
+        }
+        var splitHourPill = neededHour.split(":")
         var timePill = splitHourPill[0] + splitHourPill[1]
         var timeRealPill = timePill.toInt()
         if(timeRealPill <= 1100){
             morningPills.add(pill)
+            morningString.add(neededHour)
         }
         if(timeRealPill > 1100 && timeRealPill <= 1600) {
             afternoonPills.add(pill)
+            afternoonString.add(neededHour)
         }
         if (timeRealPill > 1600) {
             eveningPills.add(pill)
+            eveningString.add(neededHour)
         }
-        Log.d(TAG, "Name of pill: ${ pill.name }")
     }
+        }
 
 
 
@@ -121,26 +206,41 @@ fun MyUi(navController: NavController, pillViewModel: PillDetailViewModel, appoi
         Text(text = currentTime, color = Color.Black, fontSize = 50.sp)
         Text(text = "Morning", color = Color.Black, fontSize = 30.sp)
 
+<<<<<<< HEAD
         if (appointmentList != null) {
             for (app in appointmentList){
                 //showAppointment(datetime = app.dateTime, hospital = app.hospital)
+=======
+            for (app in todaysMorningAppointments){
+                showAppointment(datetime = app.dateTime, hospital = app.hospital)
+>>>>>>> 03ed28f (idk)
             }
+        var i = 0
+        while (i <= morningPills.size){
+
+            showPill(morningPills[i].name, morningPills[i].days, morningString[i])
+            i++
         }
-        for (pill in morningPills){
-            Log.d(TAG, pill.name)
-            showPill(pill.name, pill.days, pill.hour)
-        }
-        //showPill("namejfij", "days", "08:30")
-        //showPill("namejfij", "days", "23:50")
 
 
         Text(text = "Afternoon", color = Color.Black, fontSize = 30.sp)
-        for (pill in afternoonPills){
-            showPill(pill.name, pill.days, pill.hour)
+        for (app in todaysAfternoonAppointments){
+            showAppointment(datetime = app.dateTime, hospital = app.hospital)
         }
+        var j = 0
+        while (j <= afternoonPills.size){
+            showPill(afternoonPills[i].name, afternoonPills[i].days, afternoonString[i])
+            j++
+        }
+
         Text(text = "Evening", color = Color.Black, fontSize = 30.sp)
-        for (pill in eveningPills){
-            showPill(pill.name, pill.days, pill.hour)
+        for (app in todaysEveningAppointments){
+            showAppointment(datetime = app.dateTime, hospital = app.hospital)
+        }
+        var l = 0
+        while (l <= eveningPills.size){
+            showPill(eveningPills[i].name, eveningPills[i].days, eveningString[i])
+            l++
         }
 
 
